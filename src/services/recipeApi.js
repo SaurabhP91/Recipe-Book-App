f5291e7afa4f40c2210e04410594810037ddd77c
saura <saurabhpatra14114@gmail.com>
2024-03-22 12:04:37 +0530

    updated api key

--- src/services/recipeApi.js
+++ src/services/recipeApi.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
 
+//primary account
+//export const api_key = 'ddd9e82634ca4bb997105a46b662d042';
 
-export const api_key = 'ddd9e82634ca4bb997105a46b662d042';
+//secondary
 //export const api_key = '26c4bc2973564edba75978929fa3bb7b';
+
+//submission
+export const api_key = 'cfeff19fcd3e419abfe904fadfc03df3';
 export const baseURL = 'https://api.spoonacular.com/recipes/';
